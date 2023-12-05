16be3984c390af9ca10af8afcc3f4d88de4628da
sreeja0111 <sreeja01103@gmail>
2023-12-05 12:38:55 +0530

    Mavenjavapush

---
+++ src/main/java/sayhello/sample1java/App.java
@@ -0,0 +1,13 @@
+package sayhello.sample1java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sayhello/sample1java/AppTest.java
@@ -0,0 +1,20 @@
+package sayhello.sample1java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
